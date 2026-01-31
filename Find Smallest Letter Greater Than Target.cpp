bb4f1cf893f6c9e0397f2ef55c4e4ccf31f8ec38
Subhasmita Sahu <subhasmita.saloni@gmail.com>
2026-01-31 08:43:00 +0530

    Solved Find Smallest Letter Greater Than Target.cpp

---
+++ Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(auto ch : letters){
+            if(ch > target){
+                return ch;
+            }
+        }
+        return letters[0];
+    }
+};
